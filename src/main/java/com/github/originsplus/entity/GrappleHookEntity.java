aa69f0eb61a89e4e3a49908a7e79297bd505c045
lochnessdragon <lochnessdragon@github.com>
2021-03-09 14:51:29 -0500

    Added Grapple Entity Type

--- src/main/java/com/github/originsplus/entity/GrappleHookEntity.java
+++ src/main/java/com/github/originsplus/entity/GrappleHookEntity.java
@@ -2,18 +2,27 @@ package com.github.originsplus.entity;
 
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.projectile.FishingBobberEntity;
 import net.minecraft.entity.projectile.thrown.ThrownItemEntity;
 import net.minecraft.item.Item;
 import net.minecraft.world.World;
 
-public class GrappleHookEntity extends FishingBobberEntity {
+public class GrappleHookEntity extends ThrownItemEntity {
 
-	public GrappleHookEntity(PlayerEntity thrower, World world, int lureLevel, int luckOfTheSeaLevel) {
-		super(thrower, world, lureLevel, luckOfTheSeaLevel);
+	public GrappleHookEntity(EntityType<? extends ThrownItemEntity> entityType, World world) {
+		super(entityType, world);
 	}
-
-
+ 
+	public GrappleHookEntity(World world, LivingEntity owner) {
+		super(null, owner, world); // null will be changed later
+	}
+ 
+	public GrappleHookEntity(World world, double x, double y, double z) {
+		super(null, x, y, z, world); // null will be changed later
+	}
+ 
+	@Override
+	protected Item getDefaultItem() {
+		return null; // We will configure this later, once we have created the ProjectileItem.
+	}
 
 }
